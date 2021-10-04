35d6870913e9340bcfdadc5e6939790c08d80b58
Brandon <brandon121j@gmail.com>
2021-10-04 14:26:05 -0400

    Refactoring of checkIsEmpty

--- routes/users/controller/userController.js
+++ routes/users/controller/userController.js
@@ -4,45 +4,48 @@ const User = require('../model/User');
 
 const {
     isAlpha,
-    isAlphaNumeric,
+    isAlphanumeric,
     isEmpty,
     isEmail,
     isStrongPassword
 } = require('validator');
 
-async function createUser(req, res) {
-    const { firstName, lastName, username, email, password } = req.body;
-    let body = req.body;
+function validRegister(value) {
     let errObj = {};
 
-    if (!isAlpha(firstName)) {
-        errObj.firstName = "First Name cannot have special characters or numbers";
+    if (!isAlpha(value.firstName)) {
+        errObj.firstName = "First name cannot have any special characters or numbers"
     }
-    
-    if (!isAlpha(lastName)) {
-        errObj.lastName = "Last Name cannot have special characters or numbers";
+
+    if (!isAlpha(value.lastName)) {
+        errObj.lastName = "Last name cannot have any special characters or numbers"
     }
-    
-    if (!isAlphaNumeric(username)) {
-        errObj.username = "Username cannot have special characters";
+
+    if (!isAlphanumeric(value.username)) {
+        errObj.username = "Username cannot have special characters"
     }
-    
-    if (!isEmail(email)) {
-        errObj.email = "please enter a valid email";
+
+    if (!isEmail(value.email)) {
+        errObj.email = "Please enter a valid email"
     }
-    
-    if (!isStrongPassword(password)) {
-        errObj.password =
-        "Your password must contain 1 lowercase, 1 uppercase, 1 number, 1 special character and at least 8 characters long";
+
+    if (!isStrongPassword(value.password)) {
+        errObj.password = 
+        "Your password must contain 1 lowercase, 1 uppercase, 1 special character"
     }
 
     if (Object.keys(errObj).length > 0) {
-        return res.status(500).json({
-            message: "ERROR",
-            error: errObj
-        });
+        return res
+            .status(500)
+            .json({ message: "ERROR", error: errObj })
     }
+}
 
+async function createUser(req, res) {
+    let body = req.body;
+    const { firstName, lastName, username, email, password } = body;
+
+    validRegister(body);
     try {
         let salt = await bcrypt.genSalt(10)
         let hashed = await bcrypt.hash(password, salt)
@@ -53,48 +56,18 @@ async function createUser(req, res) {
             email,
             password: hashed
         })
-        let savedUser = awaitcreatedUser.save()
-
-        res.json({ message: "SUCCESS", payload: savedUser })
+        let savedUser = await createdUser.save();
+        res.json({ message: "SUCCESS", savedUser })
     } catch(error) {
-        res.status(500).json({ message: "FAILURE", error: error.message })
-    }  
+        res.status(500).json({ message: "FAILURE", error: error.message})
+    }
 }
 
 async function login(req, res) {
-    const { email, password } = req.body
 
-    let errObj = {}
-
-    if (isEmpty(password)) {
-        errObj.password = "Password cannot be empty"
-    }
-
-    if (isEmpty(email)) {
-        errObj.email = "Email cannot be empty"
-    }
-
-    if (!isEmail(email)) {
-        errObj.email = "Please enter a valid email"
-    }
-
-    if (Object.keys(errObj).length > 0) {
-        return res.status(500).json({
-            message: "ERROR",
-            error: errObj
-        })
-    }
-
-    let foundUser = await User.findOne({ email: email, password: password })
-
-    res.json({ foundUser })
-
-    try {
-
-    } catch(e) {
-        res.status(500).jason({ message: "ERROR", error: error.message })
-    }
 }
+
+
 
 module.exports = {
     createUser,
--- routes/users/usersRouter.js
+++ routes/users/usersRouter.js
@@ -2,13 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 const { createUser, login } = require('./controller/userController')
-
+const { checkIsEmpty } = require('./lib/authMiddleWare/checkIsEmpty');
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-router.post('/create-user', createUser);
-router.post('/login', login);
+router.post('/create-user', checkIsEmpty, createUser);
+router.post('/login', checkIsEmpty, login);
 
 module.exports = router;
