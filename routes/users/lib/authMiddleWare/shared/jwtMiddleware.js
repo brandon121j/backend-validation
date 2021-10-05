22796c1fb7186c89b07406d781f7cbc31bc4b8ab
Brandon <brandon121j@gmail.com>
2021-10-05 15:45:28 -0400

    Completion of jwtMiddleware function

--- routes/users/lib/authMiddleWare/shared/jwtMiddleware.js
+++ routes/users/lib/authMiddleWare/shared/jwtMiddleware.js
@@ -2,9 +2,21 @@ const jwt = require('jsonwebtoken');
 
 function jwtMiddleware(req, res, next) {
     try {
-        console.log(req.headers)
+        console.log(req.headers.authorization)
 
-        res.json({ message: "SUCCESS"})
+        if (req.headers && req.headers.authorization) {
+            
+            let notDecoded = req.headers.authorization
+
+            let slicedToken = notDecoded.slice(7)
+
+            let decodedToken = jwt.verify(slicedToken, process.env.JWT_SECRET)
+
+            res.json({ message: decodedToken })
+        } else {
+            throw ({ message: "You don't have permission"})
+        }
+        res.json({ message: "SUCCESS", token: decodedToken })
     } catch(e) {
         res
             .status(500)
